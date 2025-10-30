159168864f69bb766f12ade5d87cbc46b1a7abe7
Bilyana Hristova <bilyanahristova@Bilyanas-MacBook-Air.local>
2025-10-30 14:34:02 +0200

    ref(swag-labs): refine pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,14 @@ export default defineConfig({
   testDir: './tests',
   workers: 1,
   retries: 1,
+  forbidOnly: true,
   reporter: [['list']],
   use: {
     baseURL: 'https://www.saucedemo.com/v1/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false,
+    headless: true,
   },
   projects: [
     {
